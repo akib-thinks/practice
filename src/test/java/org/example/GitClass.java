82a29de171ef594ae6fb49ce668349bc7c79f8ae
akib619 <u1173968@internal.imsglobal.com>
2025-12-19 02:41:59 +0600

    commit without pass

--- src/test/java/org/example/GitClass.java
+++ src/test/java/org/example/GitClass.java
@@ -6,7 +6,6 @@ import org.testng.annotations.Test;
 public class GitClass {
 
     public static void main(String[] args) {
-        String password = "123asdas";
-        System.out.println("Welcome to the Git World !" + password);
+        System.out.println("Welcome to the Git World !");
     }
 }
