6191eb4156ec07a38356ec2cecfafe9cd8386a23
akib619 <u1173968@internal.imsglobal.com>
2025-12-18 22:01:39 +0600

    undo changes

--- src/test/java/org/example/GitClass.java
+++ src/test/java/org/example/GitClass.java
@@ -1,6 +1,10 @@
 package org.example;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class GitClass {
+
     public static void main(String[] args) {
         System.out.println("Welcome to the Git World !");
     }
